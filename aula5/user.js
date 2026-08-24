5d6d1a04e9ce19ebb8a6829c77c8d233b81bdd19
alice jardim <alicejardimj2006@gmail.com>
2026-08-24 19:41:44 -0300

    meu primeiro commit

---
+++ aula5/user.js
@@ -0,0 +1,64 @@
+var users =  JSON.parse(localStorage.getItem("users") ) || [];
+var logged = JSON.parse(localStorage.getItem("logged")) || {};
+var Ola = document.getElementById("Ola!") 
+
+if(Ola && logged) Ola.innerHTML = "Olá " + logged.nome 
+
+var listenerUsers = document.getElementById("listenerUsers")
+if(listenerUsers)
+        listenerUsers.innerHTML = JSON.stringify(users,null,4)
+
+
+var formR = document.getElementById("formRegister");
+formR?.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let name = document.getElementById("iName").value;
+    let email = document.getElementById("iEmail").value;
+    let senha = document.getElementById("iSenha").value;
+    let date = document.getElementById("iDate").value;
+
+    const user = {//objeto,anonimo, estrutura, json
+        nome: name,
+        email: email,
+        senha: senha,
+        nascimento: date
+    }
+
+    users.push(user)
+    localStorage.setItem("users", JSON.stringify(users))
+
+})
+
+var formL = document.getElementById("formLogin");
+formL?.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let email = document.getElementById("LoginEmail").value;
+    let senha = document.getElementById("LoginSenha").value;
+
+    let user = users.find(u => { return u.email === email 
+
+    });
+    
+    if(!user){//not usuario}     
+        console.log("usuário não encontrado")
+        return
+    }
+
+    if(user){
+         
+        if(user.senha == senha){
+            console.log("usuário logged")
+            localStorage.setItem("logged", JSON.stringify(user))
+            window.location.href = "painel.html"
+           
+        }else{
+            console.log("senha incorreta")
+            
+        }
+    }else{
+        console.log("usuário não encontrado")
+    }
+
+})
